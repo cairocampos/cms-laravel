d75593c1664d8d2b04cf7998e5b273ebe7856aa1
Cairo Campos <cairocampos98@gmail.com>
2020-09-06 14:02:36 -0300

    View share data template

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{
+{   
     public function index() {
+
+
         return view("site.home");
     }
 }
